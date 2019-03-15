aa38330d86ef058d6d089b30e862ac2cc1ec6086
BuildTools <tillson.galloway@gmail.com>
2019-03-15 18:02:14 -0400

    remove heroku

--- stupiddeploy.sh
+++ stupiddeploy.sh
@@ -1,2 +1,7 @@
-mv build/index.html build/index.php
+rm ../sort-homework-build/*
+rm -rf ../sort-homework-build/static
+yarn build
+mv ./build/* ../sort-homework-build/
+cd ../sort-homework-build
+mv index.html index.php
 echo "Ready for deployment."
